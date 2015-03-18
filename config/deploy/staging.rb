6605134030326dd37f81dfea4c6ada9cea79e964
threeaster <private@threeaster.net>
2015-03-18 18:37:49 +0900

    capistrano設定ファイルの作成

---
+++ config/deploy/staging.rb
@@ -0,0 +1,48 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, 'deploy@localhost'
+role :web, 'deploy@localhost'
+role :db,  'deploy@localhost'
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server 'localhost', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+
+set :deploy_to, '/var/www/rails/nico_market_archive_staging'
+
+set :branch, 'develop'
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+set :ssh_options, {
+   keys: %w(/var/lib/jenkins/.ssh/id_rsa),
+   forward_agent: false,
+   auth_methods: %w(publickey)
+}
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
