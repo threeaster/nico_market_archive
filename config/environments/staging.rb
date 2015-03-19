a558b577dc1571008831a63755884f8b9e61e64a
threeaster <private@threeaster.net>
2015-03-19 19:50:45 +0900

    コピペしたため、CumulativeEffortとなっていた定数をNicoMarketArchiveに変更

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-CumulativeEffort::Application.configure do
+NicoMarketArchive::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
