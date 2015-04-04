84bc12c17abab2aa6a3ead5e105ea1ae4c1f30f2
山田大地 <private@threeaster.net>
2015-04-04 18:33:26 +0900

    productのshop_idにenum設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  has_and_belongs_to_many :history
+  enum shop_id: { amazon: 1 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'enum' do
+    describe 'amazonはproduct_idが1である' do
+      let!(:product){ create :product, shop_id: Product.shop_ids[:amazon] }
+      let(:products){ Product.amazon }
+
+      it { expect(products.count).to eq 1 }
+      it { expect(products.first.product_id).to eq product.product_id }
+    end
+  end
 end
