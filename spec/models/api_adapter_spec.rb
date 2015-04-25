fe73f9b1d6e73a57ee37179392f7032f9635223d
山田大地 <private@threeaster.net>
2015-04-25 14:48:43 +0900

    テストの修正し忘れ

--- spec/models/api_adapter_spec.rb
+++ spec/models/api_adapter_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe ApiAdapter do
   describe '.get_market_info' do
     let(:expected) do
-      product_ids = %w[4056000816 B000P5FHCQ B000CQCOLA B00014B10S B0044BIQDO B009K2N4FW B009K2QJ0E]
+      product_ids = %w[az4056000816 azB000P5FHCQ azB000CQCOLA azB00014B10S azB0044BIQDO azB009K2N4FW azB009K2QJ0E]
       product_ids.map{ |product_id| { shop_id: 1, product_id: product_id } }
     end
 
