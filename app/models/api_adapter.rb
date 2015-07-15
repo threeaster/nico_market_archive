035324a54d9684239abcfdd20953550c01d97295
山田大地 <private@threeaster.net>
2015-07-16 02:03:05 +0900

    api_adapterで取れる情報を増やした

--- app/models/api_adapter.rb
+++ app/models/api_adapter.rb
@@ -4,14 +4,28 @@ class ApiAdapter
   def self.get_market_info movie_id
     url = "http://ichiba.nicovideo.jp/embed/zero/show_ichiba?v=#{movie_id}"
     main_html = open(url){ |f| Nokogiri::HTML.parse JSON.parse(f.read)['main'] }
-    items = main_html.css 'div'
-    items.select{ |item|
-      item.attribute('id').to_s.start_with? 'ichibaitem_watch'
-    }.map do |item|
-      { shop_id: 1, 
-        product_id: item.attribute('id').to_s.sub('ichibaitem_watch_', ''),
-        product_name: item.css('img').first.attribute('alt').to_s,
-        product_image_url: item.css('img').first.attribute('src').to_s }
+    items = main_html.css 'dl.ichiba_mainitem'
+    items.map do |item|
+      ichibaitem_watch = item.css('div[id^=ichibaitem_watch_]').first
+      {
+        shop_id: 1,
+        product_id: ichibaitem_watch.attribute('id').to_s.sub('ichibaitem_watch_', ''),
+        product_name: ichibaitem_watch.css('img').first.attribute('alt').to_s,
+        product_image_url: ichibaitem_watch.css('img').first.attribute('src').to_s,
+        maker: item.css('dd.maker').first.content,
+        buy_num: get_num(item.css('span.buy').first),
+        clicked_num: get_num(item.css('span.click').first),
+        clicked_at_this_movie: get_num(item.css('dd.action span').reject{ |span| ['buy', 'click'].include? span.attribute('class').to_s }.first)
+      }
+    end
+  end
+
+  private
+  def self.get_num span
+    if span
+      span.content.sub(',', '').sub('人', '').to_i
+    else
+      0
     end
   end
 end
--- spec/models/api_adapter_spec.rb
+++ spec/models/api_adapter_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 describe ApiAdapter do
   describe '.get_market_info' do
-    let(:expected) do
-      product_ids = %w[az4056000816 azB000P5FHCQ azB000CQCOLA azB00014B10S azB0044BIQDO azB009K2N4FW azB009K2QJ0E]
-      product_names = [
+    let(:shop_ids){ Array.new(7, 1) }
+    let(:product_ids){ %w[az4056000816 azB000P5FHCQ azB000CQCOLA azB00014B10S azB0044BIQDO azB009K2N4FW azB009K2QJ0E] }
+    let(:product_names) do
+      [
         '陰陽道の本―日本史の闇を貫く秘儀・占術の系譜 (NEW SIGHT MOOK Books Esoterica 6)',
         '新豪血寺一族-煩悩解放-(DVD付)',
         '新・豪血寺一族-煩悩解放-',
         '新豪血寺一族 闘婚 NG 【NEOGEO】',
         'レッツゴー！陰陽師',
         '陰陽師　安倍　晴明風　  男性M    コスプレ衣装　 　完全オーダメイドも対応',
-        '陰陽師  彰子風　  女性M    コスプレ衣装　 　完全オーダメイドも対応asterInPla'
+        '陰陽師  彰子風　  女性M    コスプレ衣装　 　完全オーダメイドも対応'
       ]
-      product_image_urls = %w[
+    end
+    let(:product_image_urls) do
+      %w[
         http://ecx.images-amazon.com/images/I/51H24893PSL._AA178_.jpg
         http://ecx.images-amazon.com/images/I/31GhQQVVoEL._AA178_.jpg
         http://ecx.images-amazon.com/images/I/31ZFo3xRGtL._AA178_.jpg
@@ -21,15 +24,54 @@ describe ApiAdapter do
         http://ecx.images-amazon.com/images/I/41RGc7k2rUL._AA178_.jpg
         http://ecx.images-amazon.com/images/I/41DecVejRuL._AA178_.jpg
       ]
-      product_ids.each_with_index.map{ |product_id, i| { shop_id: 1, product_id: product_id, product_name: product_names[i], product_image_url: product_image_urls[i] } }
+    end
+    let(:makers) do
+      ['学研マーケティング', 'ゲーム・サントラ,矢部野彦磨&琴姫 With 坊主ダンサー,秋葉原三人娘,惑星ペンダギンのサダ吉とその仲間達,無一文隼人', 'エキサイト', 'プレイモア', 'Ｎｅ−Ｈｏ', 'LUGANO', 'LUGANO'
+      ]
+    end
+    let(:buy_nums){ [12, 340, 28, 0, 0, 0, 0] }
+    let(:clicked_nums){ [237, 22631, 43985, 1414, 225, 0, 0] }
+    let(:clicked_at_this_movies){ [7, 1, 3, 2, 1, 0, 0] }
+    let(:actual){ ApiAdapter.get_market_info 'sm9' }
+
+    def actual_attr(attribute)
+      actual.map{ |h| h[attribute] }
     end
 
     before do
       stub_request(:get, "http://ichiba.nicovideo.jp/embed/zero/show_ichiba?v=sm9").to_return(:status => 200, :body => page_file('sm9_market.json'))
     end
 
-    it '与えられた動画IDに対して、その市場に登録されている商品の店舗IDと商品情報のhashの配列が帰ってくる' do
-      expect(ApiAdapter.get_market_info 'sm9').to eq expected
+    it 'shop_idたちを取得できる' do
+      expect(actual_attr :shop_id).to eq Array.new(7, 1)
+    end
+
+    it 'product_idたちを取得できる' do
+      expect(actual_attr :product_id).to eq product_ids
+    end
+
+    it 'product_namesたちを取得できる' do
+      expect(actual_attr :product_name).to eq product_names
+    end
+
+    it 'product_image_urlたちを取得できる' do
+      expect(actual_attr :product_image_url).to eq product_image_urls
+    end
+
+    it 'makerたちを取得できる' do
+      expect(actual_attr :maker).to eq makers
+    end
+
+    it 'buy_numたちを取得できる' do
+      expect(actual_attr :buy_num).to eq buy_nums
+    end
+
+    it 'clicked_numたちを取得できる' do
+      expect(actual_attr :clicked_num).to eq clicked_nums
+    end
+
+    it 'clicked_at_this_movieたちを取得できる' do
+      expect(actual_attr :clicked_at_this_movie).to eq clicked_at_this_movies
     end
   end
 end
